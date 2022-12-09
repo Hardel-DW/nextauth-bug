3567d71a02926e7d9a04361081c1e3901cc53c18
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2022-12-09 07:55:27 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className={'min-height-view px-[200px] mb-10'}>
+            <h1 className={'text-4xl font-bold'}>Page</h1>
+            <p className={'text-xl'}>This is the page component.</p>
+        </div>
+    );
+}
